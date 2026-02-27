d43557ff9786e980e3eb3dc8b8b777fe992c84b5
James Coliz <jcoliz@outlook.com>
2026-02-27 14:03:02 -0800

    feat(th): add additional options to acquire a token

--- src/SyntheticTH/Program.cs
+++ src/SyntheticTH/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Core;
 using Azure.Identity;
 using Azure.Messaging.EventHubs.Producer;
 using SyntheticTH;
@@ -12,6 +13,10 @@ builder.Configuration.AddTomlFile("config.toml", optional: true);
 var eventHubOptions = new EventHubOptions();
 builder.Configuration.Bind(EventHubOptions.Section, eventHubOptions);
 
+// Bind Identity configuration
+var identityOptions = new IdentityOptions();
+builder.Configuration.Bind(IdentityOptions.Section, identityOptions);
+
 // Configure Worker options
 builder.Services.Configure<WorkerOptions>(
     builder.Configuration.GetSection(WorkerOptions.Section));
@@ -21,10 +26,12 @@ builder.Services.Configure<WorkerOptions>(
 // of the application, which is best practice when events are being published or read regularly.
 builder.Services.AddSingleton(serviceProvider =>
 {
+    var credential = GetTokenCredential(identityOptions);
+    
     return new EventHubProducerClient(
         eventHubOptions.ServiceBusEndpoint,
         eventHubOptions.Name,
-        new DefaultAzureCredential());
+        credential);
 });
 
 builder.Services.AddHostedService<Worker>();
@@ -32,3 +39,35 @@ builder.Services.AddHostedService<Worker>();
 var host = builder.Build();
 
 await host.RunAsync();
+
+static TokenCredential GetTokenCredential(IdentityOptions identityOptions)
+{
+    // 1. If there is an IdentityOptions filled out, create a ClientSecretCredential and use that
+    if (identityOptions.TenantId != Guid.Empty &&
+        identityOptions.AppId != Guid.Empty &&
+        !string.IsNullOrWhiteSpace(identityOptions.AppSecret))
+    {
+        return new ClientSecretCredential(
+            identityOptions.TenantId.ToString(),
+            identityOptions.AppId.ToString(),
+            identityOptions.AppSecret);
+    }
+
+    // 2. If a Managed Identity credential is available, use that
+    try
+    {
+        var managedIdentityCredential = new ManagedIdentityCredential();
+        // Test if managed identity is available by attempting to get a token
+        // This will throw if managed identity is not available
+        var tokenRequestContext = new TokenRequestContext(new[] { "https://management.azure.com/.default" });
+        _ = managedIdentityCredential.GetToken(tokenRequestContext, default);
+        return managedIdentityCredential;
+    }
+    catch
+    {
+        // Managed identity not available, fall through to default
+    }
+
+    // 3. Use DefaultAzureCredential as a fall-back
+    return new DefaultAzureCredential();
+}
